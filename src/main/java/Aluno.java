246a3ef2e1dccb617fd5f00e13b347ff4af63af1
Matheus Victor <matheusvictor.salles@gmail.com>
2022-12-23 09:51:37 -0300

    feat: add classe Telefone

--- src/main/java/Aluno.java
+++ src/main/java/Aluno.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno {
 
     private Cpf cpf;
     private String nome;
     private Email email;
+    private final List<Telefone> telefones = new ArrayList<>();
+
+    public void adicionarTelefone(Telefone telefone) {
+        //TODO
+    }
+
+    public void adicionarTelefone(String ddd, String numero) {
+        this.telefones.add(new Telefone(ddd, numero));
+    }
+
+    public Cpf getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
+    }
 }
---
+++ src/main/java/Telefone.java
@@ -0,0 +1,35 @@
+public class Telefone {
+
+    private String ddd;
+    private String numero;
+
+    public Telefone(String ddd, String numero) {
+
+        if (ddd == null) {
+            throw new IllegalArgumentException("DDD precisa ser informado!");
+        }
+        if (numero == null) {
+            throw new IllegalArgumentException("Número precisa ser informado!");
+        }
+
+        if (!ddd.matches("^\\d{2}$")) {
+            throw new IllegalArgumentException("DDD inválido!");
+        }
+
+        if (!numero.matches("^\\d{8}|\\d{9}$")) {
+            throw new IllegalArgumentException("Número inválido!");
+        }
+
+        this.ddd = ddd;
+        this.numero = numero;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+}
