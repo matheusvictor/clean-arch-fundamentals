b9c4cee28b81ae20d018b5e44238f017c587cce7
Matheus Victor <matheusvictor.salles@gmail.com>
2022-12-23 09:52:00 -0300

    feat/test: implementa testes de unidade para validação de formato

---
+++ src/test/java/EmailTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class EmailTest {
+
+    @Test
+    void naoDeveCriarEmailComEnderecoInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> new Email(null));
+        assertThrows(IllegalArgumentException.class, () -> new Email(""));
+        assertThrows(IllegalArgumentException.class, () -> new Email("emailinvalido"));
+    }
+
+    @Test
+    void deveCriarEmailComEnderecoValido() {
+        assertEquals("joao@gmail.com", "");
+    }
+}
---
+++ src/test/java/TelefoneTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TelefoneTest {
+
+    @Test
+    void naoDeveCriarTelefoneComDDDInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> new Telefone(null, "123456789"));
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("", "123456789"));
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("1", "123456789"));
+    }
+
+    @Test
+    void naoDeveCriarTelefoneComNumeroInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("12", null));
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("12", ""));
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("12", "1234"));
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("12", "1234567890"));
+    }
+
+    @Test
+    void deveCriarTelefoneComDDDENumeroValidos() {
+        Telefone telefoneValido = new Telefone("12", "123456789");
+        assertEquals("12", telefoneValido.getDdd());
+        assertEquals("123456789", telefoneValido.getNumero());
+    }
+}
